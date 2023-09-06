d85fa7164aac8538205e9853064cb9600fb85775
João Feitoza <joaofeitoza.13@gmail.com>
2023-09-06 17:12:18 -0300

    feat: update serializers and settings/base.py

--- backend/apps/product/serializers.py
+++ backend/apps/product/serializers.py
@@ -6,16 +6,19 @@ from .models import Brand, Category, Product
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ["id", "name"]
 
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        fields = "__all__"
+        fields = ["id", "name"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    brand = BrandSerializer()
+    category = CategorySerializer()
+
     class Meta:
         model = Product
         fields = "__all__"
