ef24a7e6493cf577e12b38b21f3145fd354baac4
João Feitoza <joaofeitoza.13@gmail.com>
2023-09-06 01:55:08 -0300

    feat: register models to admin and created its serializers

--- backend/apps/product/admin.py
+++ backend/apps/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin  # noqa: F401
 
+from .models import Brand, Category, Product
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Product)
