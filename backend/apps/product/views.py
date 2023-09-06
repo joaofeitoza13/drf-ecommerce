cebf854e1c655daa0b98c7f9348cf4c1d1045788
João Feitoza <joaofeitoza.13@gmail.com>
2023-09-06 10:52:46 -0300

    feat: add Brand and Product viewsets and register them to the router

--- backend/apps/product/views.py
+++ backend/apps/product/views.py
@@ -1,8 +1,9 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Brand, Category, Product
+from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 
 
 # Create your views here.
@@ -13,6 +14,33 @@ class CategoryViewSet(viewsets.ViewSet):
 
     queryset = Category.objects.all()
 
+    @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for viewing all brands
+    """
+
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProductViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for viewing all products
+    """
+
+    queryset = Product.objects.all()
+
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
