cfe84cfc3f9bcf34efcf9f44ad4448e5ca6a1b01
João Feitoza <joaofeitoza.13@gmail.com>
2023-09-26 22:09:01 -0300

    feat: add product by id and by category endpoints

--- backend/apps/product/views.py
+++ backend/apps/product/views.py
@@ -1,5 +1,6 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Brand, Category, Product
@@ -39,8 +40,32 @@ class ProductViewSet(viewsets.ViewSet):
     """
 
     queryset = Product.objects.all()
+    lookup_field = "slug"
+
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(self.queryset.filter(slug=slug), many=True)
+        return Response(serializer.data[0])
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category_name>\w+)/all",
+        url_name="all",
+    )
+    def list_product_by_category(self, request, category_name=None):
+        """
+        An endpoint to return products by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category_name), many=True
+        )
+        if serializer.data == []:
+            return Response(
+                "Inexistent category or no product registered under this category."
+            )
         return Response(serializer.data)
