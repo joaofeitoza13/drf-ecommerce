027a0219f3b5eec6a2211eeef273f27102fb0b85
João Feitoza <joaofeitoza.13@gmail.com>
2023-09-26 22:07:07 -0300

    feat: change admin view for products

--- backend/apps/product/admin.py
+++ backend/apps/product/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin  # noqa: F401
 
-from .models import Brand, Category, Product
+from .models import Brand, Category, Product, ProductLine
+
+
+class ProductLineInline(admin.TabularInline):
+    model = ProductLine
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInline]
+
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Brand)
-admin.site.register(Product)
+admin.site.register(ProductLine)
