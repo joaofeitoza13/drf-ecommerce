6ec22b6b8166d3b50ed9b54be33c74ab938eef59
João Feitoza <joaofeitoza.13@gmail.com>
2023-09-26 22:06:12 -0300

    feat: updating model

--- backend/apps/product/models.py
+++ backend/apps/product/models.py
@@ -33,10 +33,28 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey("Category", on_delete=models.SET_NULL, null=True, blank=True)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+
+class ProductLine(models.Model):
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    sku = models.CharField(max_length=100)
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_line")
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "product_line"
+        verbose_name = "product_lines"
+        verbose_name_plural = "Product Lines"
+
+    def __str__(self):
+        return self.sku
